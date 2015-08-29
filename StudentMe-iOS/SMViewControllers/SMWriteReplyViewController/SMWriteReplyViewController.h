bebe54cdc7a08e04a618356de659cb6df3ed7168
SeanChense <chenyu380@gmail.com>
2015-08-29 23:28:10 +0800

    disable quote TC's when reply TC

--- StudentMe-iOS/SMViewControllers/SMWriteReplyViewController/SMWriteReplyViewController.h
+++ StudentMe-iOS/SMViewControllers/SMWriteReplyViewController/SMWriteReplyViewController.h
@@ -7,9 +7,17 @@
 //
 
 #import "SMBaseViewController.h"
+
+typedef NS_ENUM(NSInteger, SMWriteReplyStyle) {
+    SMWriteReplyStyleNewTopic,
+    SMWriteReplyStyleReplyTC,/*楼主的翻译 TC Topic Creator see in https://zh.wikipedia.org/zh/%E6%A5%BC%E4%B8%BB*/
+    SMWriteReplyStyleReplyOthers,
+    SMWriteReplyStyleEditPost
+};
+
 @class SMTopic;
 
 @class SMTopicReply;
 @interface SMWriteReplyViewController : SMBaseViewController
-- (instancetype)initWithReply:(SMTopicReply *)reply topic:(SMTopic *)topic;
+- (instancetype)initWithReply:(SMTopicReply *)reply topic:(SMTopic *)topic style:(SMWriteReplyStyle)style;
 @end
